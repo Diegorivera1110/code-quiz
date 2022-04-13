946f73086bd45156d1aace51ac20545cb91838d4
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-13 16:21:11 -0700

    made small edits to JS, CSS, & HTML

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,25 @@
 var highScores = document.querySelector("#highscores");
 var timerEl = document.querySelector("#timer");
-
+var mainContent = document.querySelector(".main-content")
 var startButton = document.querySelector("#start-btn");
+var questionsList;
 
 
+// handles the action after starting quiz
 var buttonHandler = function(event) {
   var buttonEl = event.target;
   
-  if (buttonEl )
+  if (buttonEl.matches("#start-btn")) {
+    start();
+  } else if ()
 };
+  
+  // the function actions after quiz has been started
+  var start = function() {
+    var timer = setInterval(function() {
+      timerEl.textContent = "Timer: " + timeLeft;
+    })
+  }
   
   
   
